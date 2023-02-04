9c846ea66b5037e9f9efa8ffa66fe508619909c2
Ricky428 <103875348+Ricky428@users.noreply.github.com>
2023-02-04 11:02:57 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-SpringBall-Animation
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-vdqedj)
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div className="app">
+      <div className="elastic">
+        <div className="ball"></div>
+        <svg>
+          {' '}
+          <path> </path>{' '}
+        </svg>
+        <svg>
+          {' '}
+          <path> </path>{' '}
+        </svg>
+      </div>
+    </div>
+  );
+}
